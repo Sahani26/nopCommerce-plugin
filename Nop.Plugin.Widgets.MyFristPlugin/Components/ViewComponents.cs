0e14a1c3d561c43436511e630b6741e485c6bb90
Sunil Sahani <107791326+Sahani26@users.noreply.github.com>
2025-02-21 18:45:23 +0530

    Add files via upload

---
+++ Nop.Plugin.Widgets.MyFristPlugin/Components/ViewComponents.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Core;
+using Nop.Web.Framework.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nop.Plugin.Widgets.MyFristPlugin.Components
+{
+    public class ViewComponents: NopViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(string widgetZone, object additionalData)
+        { 
+            var model = "this is my first plugin"; 
+            return View("~/Plugins/Widgets.MyFristPlugin/Views/PublicInfo.cshtml", model);
+        }
+
+    }
+}
---
+++ Nop.Plugin.Widgets.MyFristPlugin/MyFristPlugin.cs
@@ -0,0 +1,35 @@
+﻿using Nop.Services.Cms;
+using Nop.Services.Plugins;
+using Nop.Web.Framework.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nop.Plugin.Widgets.MyFristPlugin
+{
+    public class MyFristPlugin:BasePlugin,IWidgetPlugin
+    {
+        public bool HideInWidgetList =>false;
+
+        public Type GetWidgetViewComponent(string widgetZone)
+        {
+            return typeof(Components.ViewComponents);
+        }
+
+        public Task<IList<string>> GetWidgetZonesAsync()
+        {
+            return Task.FromResult<IList<string>>(new List<string> { PublicWidgetZones.HeaderAfter });
+        }
+
+        public override Task InstallAsync()
+        {
+            return base.InstallAsync();
+        }
+        public override Task UninstallAsync()
+        {
+            return base.UninstallAsync();
+        }
+    }
+}
