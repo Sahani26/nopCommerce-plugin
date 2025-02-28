c044b78f2a6f8f49aec9919826b61a0eb0d0e2cb
Sunil Sahani <107791326+Sahani26@users.noreply.github.com>
2025-02-28 11:23:58 +0530

    Add files via upload
    4.70.9 print all country name fron database in Allows access without authentication

---
+++ Nop.Plugin.Widgets.NewTest/Controllers/PrintCountriesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Nop.Services.Directory;
+using Nop.Web.Framework.Controllers;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nop.Plugin.Widgets.NewTest.Controllers
+{
+    [Route("Admin/PrintCountries")]
+    public class PrintCountriesController : BasePluginController
+    {
+        private readonly ICountryService _countryService;
+
+        public PrintCountriesController(ICountryService countryService)
+        {
+            _countryService = countryService;
+        }
+
+        [AllowAnonymous] // Allows access without authentication
+        [HttpGet("Index")]
+        public async Task<IActionResult> Index()
+        {
+            var countries = (await _countryService.GetAllCountriesAsync()).Select(c => c.Name).ToList();
+            return View("~/Plugins/Widgets.NewTest/Views/PrintCountries/Index.cshtml", countries);
+        }
+    }
+}
---
+++ Nop.Plugin.Widgets.NewTest/NewTestPlugin.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Routing;
+using Nop.Services.Directory;
+using Nop.Services.Plugins;
+using Nop.Web.Framework.Menu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nop.Plugin.Widgets.NewTest
+{
+    public class NewTestPlugin:BasePlugin, IAdminMenuPlugin
+    {
+        private readonly ICountryService _countryService;
+
+        public NewTestPlugin(ICountryService countryService)
+        {
+            _countryService = countryService;
+        }
+
+        public override string GetConfigurationPageUrl()
+        {
+            return "/Admin/PrintCountries/Index";
+        }
+
+        public override Task InstallAsync()
+        {
+            return base.InstallAsync();
+        }
+
+        public async Task ManageSiteMapAsync(SiteMapNode rootNode)
+        {
+            var menuItem = new SiteMapNode()
+            {
+                SystemName = "PrintCountries",
+                Title = "Print Countries",
+                ControllerName = "PrintCountries",
+                ActionName = "Index",
+                Visible = true,
+                RouteValues = new RouteValueDictionary { { "area", "Admin" } }, // Fixed
+            };
+
+            var pluginNode = rootNode.ChildNodes.FirstOrDefault(x => x.SystemName == "Miscellaneous");
+            if (pluginNode != null)
+                pluginNode.ChildNodes.Add(menuItem);
+            else
+                rootNode.ChildNodes.Add(menuItem);
+        }
+
+
+
+
+        public override Task UninstallAsync()
+        {
+            return base.UninstallAsync();
+        }
+    }
+}
